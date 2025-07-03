e1ec0c9cd1e3a7c4832ca8598349104374dfcabc
Drake <druppl1@wgu.edu>
2025-07-03 18:39:25 -0500

    adds testing

--- main.py
+++ main.py
@@ -53,9 +53,8 @@ def go(config: DictConfig):
 
         if "basic_cleaning" in active_steps:
             _ = mlflow.run(
-                "https://github.com/drakebaylor/Project-Build-an-ML-Pipeline-Starter.git",   # must be the subproject directory
-                entry_point="basic_cleaning",       # defined in src/basic_cleaning/MLproject
-                version ='main',
+                f"{config['basic_cleaning']['cleaning_repository']}/basic_cleaning",   # must be the subproject directory
+                entry_point="main",       # defined in src/basic_cleaning/MLproject
                 parameters={
                     "input_artifact": "sample.csv:latest",
                     "output_artifact": "clean_sample.csv",
@@ -67,16 +66,30 @@ def go(config: DictConfig):
             )
 
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                f"{config['data_check']['check_repository']}/data_check",
+                entry_point="main",
+                parameters={
+                    "csv": "clean_sample.csv:latest",
+                    "ref": "clean_sample.csv:reference",
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"],
+                    "kl_threshold": config["data_check"]["kl_threshold"],
+                },
+            )
 
         if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                'main',
+                parameters = {
+                    "input":"clean_sample.csv:latest",
+                    "test_size":config['modeling']['test_size'],
+                    "random_seed":config['modeling']['random_seed'],
+                    "stratify_by":config['modeling']['stratify_by'],
+                }
+            )
+
 
         if "train_random_forest" in active_steps:
 
@@ -91,8 +104,20 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
-
-            pass
+            _ = mlflow.run(
+                f"{config['train']['training_repository']}/train_random_forest",
+                'main',
+                parameters = {
+                    "trainval_artifact":"trainval_data.csv:latest",
+                    "val_size":config['modeling']['val_size'],
+                    "random_seed":config['modeling']['random_seed'],
+                    "stratify_by":config['modeling']['stratify_by'],
+                    "rf_config":rf_config,
+                    "max_tfidf_features":config['modeling']['max_tfidf_features'],
+                    "output_artifact":"random_forest_export",
+                }
+            )
+            
 
         if "test_regression_model" in active_steps:
 
